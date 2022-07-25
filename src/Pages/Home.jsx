485221dd09cd5a7298a851853ab0207daea1bbe1
DayvidMac <dayvidmac18@gmail.com>
2022-07-24 22:12:12 -0300

    +bug fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import "./Home.css"
+import { BrowserRouter as Router, Switch, Route, Link } from 'react'
+import Register from './Register'
+import Calc from './Calc'
+import Info from './Info'
+
 function Home() {
     return (
         <>
-            <img src={require('../Images/LOGO.png')} className="logoloja" />
+            <img src={require('../Images/LOGO.png')} className="logoloja" alt='Logo' />
             < p className="lojaname">3DMC LOJA</p>
             <div className="container">
                 <a>Registrar</a>
