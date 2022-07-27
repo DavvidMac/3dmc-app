a3b0cc1992af1d208b51f29bc9e777ae28ab4d8e
DayvidMac <dayvidmac18@gmail.com>
2022-07-26 22:40:52 -0300

    adição dos campos na parte de info.

--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import "./Info.css"
 import NavBar from "../Components/NavBar"
 
-function Info ()  {
+function Info() {
     return (
         <>
-            <NavBar/>
+            <NavBar />
+            <div className="ss">
+                <h1>Informações</h1>
+                <input className="inpInfo" type="text" placeholder="Material"></input>
+                <input className="inpInfo" type="text" placeholder="Invest"></input>
+                <input className="inpInfo" type="text" placeholder="Consumo"></input>
+                <input className="inpInfo" type="text" placeholder="Custo Energia"></input>
+                <input className="inpInfo" type="text" placeholder="Salario"></input>
+                <input className="inpInfo" type="text" placeholder="Despesas"></input>
+                <input className="inpInfo" type="text" placeholder="Depreciação"></input>
+                <input className="inpInfo" type="text" placeholder="Dias"></input>
+                <input className="inpInfo" type="text" placeholder="Horas "></input>
+                <input className="inpInfo" type="text" placeholder="Primer"></input>
+            </div>
         </>
-      );
+    );
 }
- 
+
 export default Info;
